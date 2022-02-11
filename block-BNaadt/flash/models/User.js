fe472bb5bfe595c10d932ef92319ee84605bd963
john horo <johnhoro125@gmail.com>
2022-02-12 01:26:51 +0530

    logout

--- block-BNaadt/flash/models/User.js
+++ block-BNaadt/flash/models/User.js
@@ -1,6 +1,5 @@
 var mongoose = require(`mongoose`);
 var bcrypt = require(`bcrypt`);
-
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema(
@@ -8,13 +7,17 @@ var userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, minlength: 4, required: true },
+    age: Number,
+    phone: { type: String, minlength: 10, maxlength: 13 },
   },
   { timestamps: true }
 );
 
 userSchema.pre(`save`, function (next) {
   if (this.password && this.isModified(`password`)) {
+    console.log(this, `before hasing`);
     bcrypt.hash(this.password, 10, (err, hashed) => {
+      console.log(hashed, `after hashing`);
       if (err) return next(err);
       this.password = hashed;
       return next();
